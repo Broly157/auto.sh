2cfaae96528ac4c65ff76149748fc90c3dcfd912
Broly <34275803+Broly157@users.noreply.github.com>
2020-03-28 15:49:45 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -7,32 +7,29 @@
 #go get -u github.com/tomnomnom/assetfinder
 #go get -v github.com/projectdiscovery/subfinder/cmd/subfinder
 #apt-get install amass
+#git clone https://github.com/blechschmidt/massdns.git
 #git clone https://github.com/aboul3la/Sublist3r.git
 #go get -u github.com/tomnomnom/httprobe
 #go get github.com/tomnomnom/hacks/filter-resolved
 #go get -u github.com/theblackturtle/fprobe
 #git clone https://github.com/michenriksen/aquatone.git
-#mv every tool in your /usr/bin (except Sublist3r,aquatone {you have to give their location to use them perfectly})
-#Note: in case of massdns after downloading it for git 
-#1. do make , then cp the massdns to /usr/bin and copy /lists/resolvers.txt to /usr/share/wordlists/resolvers.txt
-#Note: here mass dns will only work for subdomain Enumeration
-#curl https://certspotter.com/api/v0/certs?domain=$1 | grep  -o '\[\".*\"\]' > ~/recondata/automatd/$1/certspotter{ManualCheck}.txt
-#use the above cmd if you have not configure certspotter in amass config.ini
-#------------------------------------------------------------------------------------------------------------------
-
+#---------------------------------------------------------------
 mkdir ~/recondata/automatd/$1
-cd ~/recondata/automatd/$1
-amass enum -passive -d $1 -o ~/recondata/automatd/$1/amass.txt
-findomain -t $1 -u ~/recondata/automatd/$1/findomain.txt 
-assetfinder --subs-only $1 > ~/recondata/automatd/$1/asset.txt
-subfinder -d $1 > ~/recondata/automatd/$1/subfinder.txt
-python ~/tools/Sublist3r/sublist3r.py -d $1 -o ~/recondata/automatd/$1/sublist3r.txt
-curl https://crt.sh/?q=%.$1 | grep "rms.com" | cut -d '>' -f2 | cut -d '<' -f1 | grep -v " " | sort -u > ~/recondata/automatd/$1/crt.txt
-massdns -r /usr/share/wordlists/resolvers.txt -t A -o S ~/recondata/automatd/$1/all.txt -w ~/recondata/automatd/$1/massdns.txt
-sed 's/A.*//' ~/recondata/automatd/$1/massdns.txt | sed 's/CN.*//' | sed 's/\..$//' > ~/recondata/automatd/$1/Subdomain_mass.txt
-rm ~/recondata/automatd/$1/massdns.txt
-cat ~/recondata/automatd/$1/*.txt | sort -u >> ~/recondata/automatd/$1/all.txt
-massdns -r /usr/share/wordlists/resolvers.txt -w massdns-op.txt ~/recondata/automatd/$1/all.txt
-cat ~/recondata/automatd/$1/all.txt | sort | filter-resolved | httprobe -c 40 > ~/recondata/automatd/$1/alive.txt
-cat ~/recondata/automatd/$1/alive.txt | fprobe -c 40 -v | grep ":200," > ~/recondata/automatd/$1/fprobe200.txt
-cat ~/recondata/automatd/$1/alive.txt | aquatone -scan-timeout 500 -screenshot-timeout 40000 -out ~/recondata/automatd/$1/$1
+mkdir ~/recondata/automatd/$1/findings
+mkdir ~/recondata/automatd/$1/final
+cd ~/recondata/automatd/$1/findings
+amass enum -passive -d $1 -o ~/recondata/automatd/$1/findings/amass.txt
+findomain -t $1 -u ~/recondata/automatd/$1/findings/findomain.txt 
+assetfinder --subs-only $1 > ~/recondata/automatd/$1/findings/asset.txt
+subfinder -d $1 > ~/recondata/automatd/$1/findings/subfinder.txt
+python ~/tools/Sublist3r/sublist3r.py -d $1 -o ~/recondata/automatd/$1/findings/sublist3r.txt
+curl https://crt.sh/?q=%.$1 | grep "rms.com" | cut -d '>' -f2 | cut -d '<' -f1 | grep -v " " | sort -u > ~/recondata/automatd/$1/findings/crt.txt
+cd ../~/recondata/automatd/$1/final
+cat ~/recondata/automatd/$1/findings/*.txt | sort -u >> ~/recondata/automatd/$1/findings/all.txt
+massdns -r /usr/share/wordlists/resolvers.txt -t A -o S ~/recondata/automatd/$1/findings/all.txt -w ~/recondata/automatd/$1/findings/massdns.txt
+sed 's/A.*//' ~/recondata/automatd/$1/massdns.txt | sed 's/CN.*//' | sed 's/\..$//' > ~/recondata/automatd/$1/findings/Subdomain_mass.txt
+rm ~/recondata/automatd/$1/findings/massdns.txt
+massdns -r /usr/share/wordlists/resolvers.txt -w ~/recondata/automatd/$1/final/massdns-op.txt ~/recondata/automatd/$1/findings/all.txt
+cat ~/recondata/automatd/$1/findings/all.txt | sort | filter-resolved | httprobe -c 40 > ~/recondata/automatd/$1/final/alive.txt
+cat ~/recondata/automatd/$1/final/alive.txt | fprobe -c 40 -v | grep ":200," > ~/recondata/automatd/$1/final/fprobe200.txt
+cat ~/recondata/automatd/$1/final/alive.txt | aquatone -scan-timeout 500 -screenshot-timeout 40000 -out ~/recondata/automatd/$1/final/$1
