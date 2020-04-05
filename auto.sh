6938412ce025790afd02e64c9f1a94bb0f415ad4
Broly <34275803+Broly157@users.noreply.github.com>
2020-04-05 12:42:06 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -53,20 +53,32 @@ echo "Moving into folder _Final_"
 	cd ~/recondata/automatd/$1/final
 echo "Plain massdns Scanning"
 	massdns -r /usr/share/wordlists/resolvers.txt -w massdns-op.txt ~/recondata/automatd/$1/findings/allrootsubdomains.txt
-echo "\n\n[+] Checking for alive domains..\n"
+echo "Checking for alive domains"
 	cat ~/recondata/automatd/$1/findings/allrootsubdomains.txt | sort -u | filter-resolved | httprobe -c 40 > alive.txt
 echo "Creating Header and Response body"	
 	response.sh alive.txt 
 echo "Extracting JS files from Header and Response body"	
 	cp /usr/bin/jsfiles.sh ~/recondata/automatd/$1/final
 	jsfiles.sh 
+#------------------------------------------------------------------------------------------------------------------------------------------
 echo "Extracting endpoints from JS files"
+#------------------------------------------------------------------------------------------------------------------------------------------
 	rm jsfiles.sh
-	endpoints.sh	
+	endpoints.sh
+	cd endpoints
+for domain in $(ls ~/recondata/automatd/$1/final/endpoints)
+		do
+        		for file in $(ls ~/recondata/automatd/$1/final/endpoints/$domain)
+        	do 
+                	find . -name '*.js' -exec cat {} \; >> ~/recondata/automatd/$1/final/endpoints/$domain/allendpoints.txt
+        	done
+		done
+		cd ../
+#--------------------------------------------------------------------------------------------------------------------------------------------		
 echo "fprobe Scanning started"
 	cat alive.txt | fprobe -c 40 -v | grep ":200," > fprobe200.txt
 echo "Aquatone Started"
-	cat alive.txt | aquatone -out $1
+	cat alive.txt | aquatone -scan-timeout 500 -screenshot-timeout 40000 -out $1
 echo "Finding CNAME"
 	cat  ~/recondata/automatd/$1/findings/allrootsubdomains.txt | xargs -n 1 -I{} host -t CNAME {} > CNAME.txt
 echo "Scanning for CORS"
