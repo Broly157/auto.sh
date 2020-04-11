e038e59efc2aab332388399393b9d226393053dc
Broly <34275803+Broly157@users.noreply.github.com>
2020-04-11 21:14:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ chmod +x auto.sh
 cp auto.sh /usr/bin
 cp pwords.txt /usr/bin
 echo "auto.sh Ready to use"
+mkdir ~/recondata
+mkdir ~/recondata/automatd
 #-------------------------------------------------------------#
 echo "Copying JSfileScanner.sh > /usr/bin (By dark_warlord14) https://twitter.com/dark_warlord14?s=20" #You can find the article related to this script here https://securityjunky.com/scanning-js-files-for-endpoint-and-secrets/ 
 cp JSfileScanner.sh /usr/bin
@@ -29,18 +31,18 @@ apt install wget -y
 pip install py-altdns #(By infosec_au) https://twitter.com/infosec_au?s=20 
 #------------------------------------------------------------#
 echo "Installing Findomain"
-cd ~/Broly/findomain
+cd ~/Broly/findomain*
 cargo build --release
 cp target/release/findomain /usr/bin/
 #-------------------------------------------------------------#
 echo "Installing Massdns"
-cd ~/Broly/massdns
+cd ~/Broly/massdns*
 make 
 cp /lists/resolvers.txt /usr/share/wordlists/resolvers.txt
 cp /bin/massdns /usr/bin
 #-------------------------------------------------------------#
 echo "Installing Linkfinder"
-cd ~/Broly/LinkFinder
+cd ~/Broly/LinkFinder*
 pip3 install -r requirements.txt
 python3 setup.py install
 #-------------------------------------------------------------#
