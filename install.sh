3cf064e72882149b14b5790e23237baa08a01548
Broly <34275803+Broly157@users.noreply.github.com>
2020-05-01 19:22:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ chmod +x auto.sh
 cp auto.sh /usr/bin
 echo "auto.sh Ready to use"
 mkdir ~/Broly/findomain
-mkdir ~/recondata
-mkdir ~/recondata/automatd
 cp pwords.txt ~/Broly
 #-------------------------------------------------------------#
 echo "Copying JSfileScanner.sh > /usr/bin" #(By dark_warlord14) https://twitter.com/dark_warlord14?s=20 You can find the article related to this script here https://securityjunky.com/scanning-js-files-for-endpoint-and-secrets/ 
